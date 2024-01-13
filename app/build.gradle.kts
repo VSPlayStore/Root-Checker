5529fd594b2280d5476b39af4bbaa2bb68a8ce45
Vineel Sai <mail@vineelsai.com>
2024-01-13 11:50:01 +0530

    remove google services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id ("com.android.application")
-    id ("com.google.gms.google-services")
     id ("kotlin-android")
 }
 
@@ -76,4 +75,3 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ buildscript {
     dependencies {
         classpath ("com.android.tools.build:gradle:8.2.1")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-        classpath ("com.google.gms:google-services:4.4.0")
     }
 }
 
